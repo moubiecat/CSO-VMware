e354c0f3d4ece71f6ba77a6f01d3b476a9546974
moubiecat <itsmoubiecat@gmail.com>
2026-01-28 21:33:38 +0800

    伺服器、客戶端入口點定義

---
+++ src/client.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+
+static BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+    switch (fdwReason) {
+    case DLL_PROCESS_ATTACH:
+        break;
+
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+
+    return TRUE;
+}
