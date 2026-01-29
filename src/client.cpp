cd86238a105a1a68c570512c644d6ed604ab1dd8
moubiecat <itsmoubiecat@gmail.com>
2026-01-29 22:03:50 +0800

    移除多餘的引入

--- src/client.cpp
+++ src/client.cpp
@@ -1,7 +1,4 @@
 #include <windows.h>
-#include <print>
-#include <magic_args/magic_args.hpp>
-#include "cli.h"
 #include "client.h"
 
 static BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
@@ -14,36 +11,4 @@ static BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserv
     }
 
     return TRUE;
-}
-
-int main(int argc, char** argv) {
-	//
-	// Parse command-line arguments
-	//
-	const auto args = magic_args::parse<cli::cmd_args>(argc, argv);
-	//
-	// Create a server instance listening on localhost:8080
-	//
-	cat::client client(args->host, args->port);
-	std::println("- Client listening on {}", client.ipaddress().c_str());
-	//
-	// Connect the server
-	//
-	client.connect();
-	std::println("- Client connected");
-	//
-	// Main server loop
-	//
-	while (cat::is_connect()) {
-		//
-		// Poll for incoming events
-		//
-		client.poll();
-	}
-	//
-	// Disconnect the server
-	//
-	client.disconnect();
-	std::println("- Client disconnected\n");
-	return 0;
 }
