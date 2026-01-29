6bacde5715c8e7be697f7711fd07c4a9dfcd7ee8
hongyi.jian <hongyi.jian@gigabyte.com>
2026-01-29 11:27:14 +0800

    完成 srv 的 cli 介面

--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,36 @@
+#include <print>
+#include <magic_args/magic_args.hpp>
+#include "cli.h"
+#include "server.h"
+
 int main(int argc, char* argv[]) {
+	//
+	// Parse command-line arguments
+	//
+	const auto args = magic_args::parse<cli::cmd_args>(argc, argv);
+	//
+	// Create a server instance listening on localhost:8080
+	//
+	cat::server srv(args->host, args->port);
+	std::println("- Server listening on {}", srv.ipaddress().c_str());
+	//
+	// Connect the server
+	//
+	srv.connect();
+	std::println("- Server connected");
+	//
+	// Main server loop
+	//
+	while (cat::is_connect()) {
+		//
+		// Poll for incoming events
+		//
+		srv.poll();
+	}
+	//
+	// Disconnect the server
+	//
+	srv.disconnect();
+	std::println("- Server disconnected\n");
 	return 0;
 }
