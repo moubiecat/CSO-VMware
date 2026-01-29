1ea62fdf053b912374bbdc7dc88858d13fabe51a
hongyi.jian <hongyi.jian@gigabyte.com>
2026-01-29 11:37:18 +0800

    完成 client 端 cli 介面，並完成客戶端連線請求功能

--- src/core.cpp
+++ src/core.cpp
@@ -132,6 +132,13 @@ namespace cat::core {
 		if (conn == nullptr) {
 			throw std::runtime_error("No available peers for initiating an ENet connection.");
 		}
+
+		int res = enet_host_service(host, nullptr, 5000);
+		if (res <= 0 || conn->state != ENET_PEER_STATE_CONNECTED) {
+			enet_peer_reset(conn);
+			conn = nullptr;
+			throw std::runtime_error("Connection to ENet server failed or timed out.");
+		}
 	}
 
 	/*
